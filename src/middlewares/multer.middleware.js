b42b2eff17d1e04dd0378548e5157e33bc91f45b
rohitseth01 <rohitseth010@gmail.com>
2025-02-01 16:27:48 +0530

    all setup configured

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,16 @@
+import multer from "multer";
+
+//multer is used as  middle ware//lecture-10  //localpath me file tore karake fir cloudinary me upload karenge
+
+const storage=multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,"./public/temp")
+    },
+    filename:function(req,file,cb){
+        cb(null,file.originalname)
+    }
+})
+
+export const upload=multer({
+    storage,
+})
